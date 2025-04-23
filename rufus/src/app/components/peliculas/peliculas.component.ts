8342a500923093639af1165787879367c270ca8d
jenn8199 <jennihernandez8199@gmail.com>
2025-04-22 20:09:53 -0600

    cambios de comentarios

--- rufus/src/app/components/peliculas/peliculas.component.ts
+++ rufus/src/app/components/peliculas/peliculas.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { RouterModule } from '@angular/router'; // 👈 IMPORTANTE
+import { RouterModule } from '@angular/router'; 
 import { HeaderComponent } from '../header/header.component';
 
 declare function initCustomScripts(): void;
@@ -7,7 +7,7 @@ declare function initCustomScripts(): void;
 @Component({
   selector: 'app-peliculas',
   standalone: true,
-  imports: [RouterModule, HeaderComponent], // 👈 AGREGA RouterModule
+  imports: [RouterModule, HeaderComponent], 
   templateUrl: './peliculas.component.html',
   styleUrls: ['./peliculas.component.css']
 })
